828d2efab06b02439abc18760f9a468c850a2092
Dhruv Patel <dhruv1947patel@gmail.com>
2023-03-26 16:01:11 -0400

    Account class
    Small changes in account class for better functionality.

--- src/tmu/bank/Account.java
+++ src/tmu/bank/Account.java
@@ -6,6 +6,7 @@ package com.mycompany.tmubank;
 
 import com.mycompany.tmubank.BankDatabase.DatabaseCon;
 import java.util.ArrayList;
+import javax.swing.JOptionPane;
 /**
  *
  * @author dhruv
@@ -25,31 +26,55 @@ public class Account {
         this.balance = 0;
         this.transactionHistory = new ArrayList<>();
     }
-
+    
+    /*
+    * return the User class object
+    */
     public User getU() {
         return U;
     }
 
+    /*
+    * set the User class object
+    * @params u 
+    */
     public void setU(User u) {
         this.U = u;
     }
 
+    /*
+    * return balance of the balance
+    */
     public double getBalance() {
         return balance;
     }
-
+    
+    /*
+    * set the balance 
+    */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /*
+    * return transactionHistory arraylist
+    * that has transaction details.
+    */
     public ArrayList<Transaction> getTransactionHistory() {
         return transactionHistory;
     }
 
+    /*
+    * set the transactionHistory
+    */
     public void setTransactionHistory(ArrayList<Transaction> transactionHistory) {
         this.transactionHistory = transactionHistory;
     }
 
+    /*
+    * deposit the amount and return true if it is true
+    * as false
+    */
     public boolean deposit(double amount){
         DatabaseCon dbcon = new DatabaseCon();
         this.balance += amount;
@@ -59,19 +84,40 @@ public class Account {
         return dbcon.depositUser(this,getU());
     }
 
+    /*
+    * withdraw the the given amount
+    */
     public void withdraw(double amount) {
-        balance -= amount;
-        Transaction transaction = new Transaction(amount, "Withdrawal");
-        transactionHistory.add(transaction);
+        if(amount < balance){
+            balance -= amount;
+            Transaction transaction = new Transaction(amount, "Withdrawal");
+            transactionHistory.add(transaction);
+        }
+        else{
+            System.out.println("Withdraw failed due insufficient balance");
+            JOptionPane.showMessageDialog(null, "Withdraw failed due insufficient balance", "Title", 1);
+        }
+            
     }
 
+    /*
+    * transfer the amount between the users
+    */
     public void transfer(double amount, Account recipient) {
-        balance -= amount;
-        recipient.setBalance(recipient.getBalance() + amount);
-        Transaction transaction = new Transaction(amount, "Transfer to " + recipient.getU().getUserUsername());
-        transactionHistory.add(transaction);
-        recipient.getTransactionHistory().add(transaction);
+        if(amount < balance){
+            balance -= amount;
+            recipient.setBalance(recipient.getBalance() + amount);
+            Transaction transaction = new Transaction(amount, "Transfer to " + recipient.getU().getUserUsername());
+            transactionHistory.add(transaction);
+            recipient.getTransactionHistory().add(transaction);
+        }
+        else{
+            System.out.println("Tranaction failed due insufficient balance");
+            JOptionPane.showMessageDialog(null, "Tranaction failed due insufficient balance", "Error", 1);
+        }
+        
     }
+    
     /**
      * get the account type of user
      * @return
